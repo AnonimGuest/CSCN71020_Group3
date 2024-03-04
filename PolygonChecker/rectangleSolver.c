5bb3a6fd6e27cb9e5f5c173445490046602bed92
andyg <andyg@DESKTOP-2SJEPRJ>
2024-03-04 11:18:13 -0500

    Added unit test for invalid input

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool isRectangle(int points) {
+/*
+The second approach is to write code to determine which "corner" each point is, and
+then connect the corners to form the shape. Using this approach, no matter the
+order the points are entered, no lines will cross. This approach is worth extra marks.*/
+
+bool isRectangle(int points[2][4]) {
+
 	return true;
+
 }
