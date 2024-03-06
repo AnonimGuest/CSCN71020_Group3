95d2d77166ab1aa81559a14dd6c46947c0e478ad
LexBwmn <148971701+LexBwmn@users.noreply.github.com>
2024-03-06 01:07:42 -0500

    Equilateral Triangle Calculation

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 
 #include "triangleSolver.h"
+#define SumAngles 180
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
@@ -10,14 +11,20 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	}
 	else if (side1 == side2 && side1 == side3) {
 		result = "Equilateral triangle";
+		// angle calculation
+
+		int angles = SumAngles/3;
+		printf("Angles: %d\n", angles); 
 	}
 	else if ((side1 == side2 && side1 != side3) || 
 		(side1 == side3 && side1 != side2))
 	{
 		result = "Isosceles triangle";
+		// angle calculation 
 	}
 	else {
 		result = "Scalene triangle";
+		// angle calculation 
 	}
 
 	return result;
