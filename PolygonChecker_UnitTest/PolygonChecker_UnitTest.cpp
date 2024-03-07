4cd78e664153ac53149b761c24be5a274d67e733
andyg <andyg@DESKTOP-2SJEPRJ>
2024-03-06 19:47:02 -0500

    Updated linker for rectangle unit tests

--- PolygonChecker_UnitTest/PolygonChecker_UnitTest.cpp
+++ PolygonChecker_UnitTest/PolygonChecker_UnitTest.cpp
@@ -4,7 +4,7 @@
 
 extern "C" int* getTriangleSides(int* triangleSides);
 extern "C" char* analyzeTriangle(int side1, int side2, int side3);
-extern "C" int isRectangle(int points[2][4]);
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -22,23 +22,7 @@ namespace PolygonCheckerUnitTest
 		}
 
 
-//-------------RECTANGLE SOLVER TESTS--------------//
-		TEST_METHOD(isRectangleTest_validInput_returnsTrue)
-		{
-			int points[2][4] = {(1,2), (3,4), (4,5), (6,7)};
-			bool Expected = true;
-			bool Result = isRectangle(points);
-			Assert::AreEqual(Expected, Result);
 
-		}
-
-		TEST_METHOD(isRectangleTest_invalidInput_returnsFalse)
-		{
-			int points[2][4] = { (1,2), (3,4), (-7,5), (6,7) };
-			bool Expected = false;
-			bool Result = isRectangle(points);
-			Assert::AreEqual(Expected, Result);
-		}
 
 
 
