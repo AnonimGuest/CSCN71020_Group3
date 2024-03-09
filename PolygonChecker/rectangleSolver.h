d5287d6788bb267f9076c0bcd8869b8bd1f0a870
harjasb <harjasbrar5@gmail.com>
2024-03-09 15:03:58 -0500

    Removed an unnecessary library from main.c

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -1,6 +1,7 @@
 #pragma once
 bool isRectangle(int points[4][2]);
 #define NUMBER_OF_POINTS 4
+#include <math.h>
 bool gatherAndCheckPoints(int points[NUMBER_OF_POINTS][2]);
 bool isRectangle(int points[4][2]);
 int pointsArray[NUMBER_OF_POINTS][2];
