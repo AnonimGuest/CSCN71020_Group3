d6e148c09c237bd37aeae1ceaa88461bfb073760
andy-iio <andyguest12@gmail.com>
2024-03-09 14:45:35 -0500

    added side length code for rectangle

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -9,31 +9,32 @@ int side = 0;
 
 int main() {
 	int pointsArray[4][2] = { {2, 3}, {3, 4}, {5, 6}, {8, 9} };
-	isRectangle(pointsArray);
-	//bool continueProgram = true;
-	//while (continueProgram) {
-	//	printWelcome();
+	bool ans = isRectangle(pointsArray);
+	printf("%d", ans);
+	bool continueProgram = true;
+	while (continueProgram) {
+		printWelcome();
 
-	//	int shapeChoice = printShapeMenu();
+		int shapeChoice = printShapeMenu();
 
-	//	switch (shapeChoice)
-	//	{
-	//	case 1:
-	//		printf_s("Triangle selected.\n");
-	//		int triangleSides[3] = { 0, 0, 0 };
-	//		int* triangleSidesPtr = getTriangleSides(triangleSides);
-	//		//printf_s("! %d\n", triangleSidesPtr[0]);
-	//		char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
-	//		printf_s("%s\n", result);
-	//		break;
-	//	case 0:
-	//		continueProgram = false;
-	//		break;
-	//	default:
-	//		printf_s("Invalid value entered.\n");
-	//		break;
-	//	}
-	//}
+		switch (shapeChoice)
+		{
+		case 1:
+			printf_s("Triangle selected.\n");
+			int triangleSides[3] = { 0, 0, 0 };
+			int* triangleSidesPtr = getTriangleSides(triangleSides);
+			//printf_s("! %d\n", triangleSidesPtr[0]);
+			char* result = analyzeTriangle(triangleSidesPtr[0], triangleSidesPtr[1], triangleSidesPtr[2]);
+			printf_s("%s\n", result);
+			break;
+		case 0:
+			continueProgram = false;
+			break;
+		default:
+			printf_s("Invalid value entered.\n");
+			break;
+	}
+	}
 	return 0;
 }
 
