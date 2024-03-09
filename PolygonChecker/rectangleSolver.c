ed985bbec655ccdd3634e7ce7eb31e645120cfc3
andy-iio <andyguest12@gmail.com>
2024-03-09 14:33:45 -0500

    code to solve rectangle corners

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,91 +1,62 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/*
-* Add a new feature to the application program to accept four points. Each point is an x,y pair.
-You may ask the user to enter the x and y one at a time, or at once and split the input yourself.
-There should be some validation on the user input to ensure you are given proper values. Once
-given the four points, generate four lines and determine if these lines form a rectangle. You
-may find it useful to re-use your triangle angle code to assist this, but you may use a unique
-function to determine if the lines make a rectangle. Report the shape's perimeter, and if it is a
-rectangle also report its area.
-
-The second approach is to write code to determine which "corner" each point is, and
-then connect the corners to form the shape. Using this approach, no matter the
-order the points are entered, no lines will cross. This approach is worth extra marks.*/
-
 #define NUMBEROFPOINTS 4
-//define x and y to replace 0 and 1 later
 
 //function definitions
-int findCornerArray(int points[4][2]);
+void findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int top_left[2], int top_right[2]);
+float calculateSideLength(int point1[2], int point2[2]);
 
 bool isRectangle(int points[4][2]) {
-	findCornerArray(points);
+	int bottom_left[2];
+	int bottom_right[2];
+	int top_left[2];
+	int top_right[2];
+
+	findCorners(points, bottom_left, bottom_right, top_left, top_right);
+
+	float side3 = calculateSideLength(top_left, top_right);
+	float side4 = calculateSideLength(top_right, bottom_right);
+	float side1 = calculateSideLength(bottom_right, bottom_left);
+	float side2 = calculateSideLength(bottom_left, top_left);
+
 	return true;
 }
 
-int findCornerArray(int points[4][2]) {
-	//identifiying the corners:
-	//top-left:min x, max y
-	//top-right:max x, max y
-	//bottom-left:min x, min y
-	//bottom-right: max x, min y
+//identifiying the corners
+void findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int top_left[2], int top_right[2]) {
+	//top-left:min x, max y, top-right:max x, max y
+	//bottom-left:min x, min y, bottom-right: max x, min y
 	//setting initial min and max coordinates
 	int minX = points[0][0];
 	int minY = points[0][1];
 	int maxX = points[0][0];
 	int maxY = points[0][1];
 
-	//setting actual min and max x&y 
-	//can reduce with ? : format later
+	//setting actual min and max x&y
 	for (int array_counter = 0; array_counter < NUMBEROFPOINTS; array_counter++) {
-		if (points[array_counter][0] < minX) {
-			minX = points[array_counter][0];
-		}
-		if (points[array_counter][1] < minY) {
-			minY = points[array_counter][1];
-		}
-		if (points[array_counter][0] > maxX) {
-			maxX = points[array_counter][0];
-		}
-		if (points[array_counter][1] > maxY) {
-			maxY = points[array_counter][1];
-		}
+		minX = points[array_counter][0] < minX ? points[array_counter][0] : minX;
+		minY = points[array_counter][1] < minY ? points[array_counter][1] : minY;
+		maxX = points[array_counter][0] > maxX ? points[array_counter][0] : maxX;
+		maxY = points[array_counter][1] > maxY ? points[array_counter][1] : maxY;
 	}
-	//for debugging (remove later)
+	//for debugging - remove later
 	printf("%d, %d, %d, %d", maxX, maxY, minX, minY);
 
-	//find which corner each is and put into a corner array
-	//maybe create a struct for lines & points?? would be easier
-	int sorted_corner_array[2][4]; //IN ORDER: top-lft, top-right, bottom-left, bottom-right
-	for (int counter = 0; counter < NUMBEROFPOINTS; counter++) {
-		//top-left corner (minX, maxY)
-		if (points[counter][0] == minX && points[counter][1] == maxY) {
-			sorted_corner_array[0][0] = points[counter][0];
-			sorted_corner_array[0][1] = points[counter][1];
-		}
-		//top-right corner (maxX, maxY)
-		if (points[counter][0] == maxX && points[counter][1] == maxY) {
-			sorted_corner_array[0][0] = points[counter][0];
-			sorted_corner_array[0][1] = points[counter][1];
-		}
-		//bottom-left corner (minX, minY)
-		if (points[counter][0] == minX && points[counter][1] == minY) {
-			sorted_corner_array[0][0] = points[counter][0];
-			sorted_corner_array[0][1] = points[counter][1];
-		}
-		//bottom-right corner (maxX, minY)
-		if (points[counter][0] == maxX && points[counter][1] == minY) {
-			sorted_corner_array[0][0] = points[counter][0];
-			sorted_corner_array[0][1] = points[counter][1];
-		}
-	}
-	return sorted_corner_array;
+	//set the corners values 
+	bottom_left[0] = minX;
+	bottom_left[1] = minY;
+	bottom_right[0] = maxX;
+	bottom_right[1] = minY;
+	top_left[0] = minX;
+	top_left[1] = maxY;
+	top_right[0] = maxX;
+	top_right[1] = maxY;
 }
 
-
-
+float calculateSideLength(int point1[2], int point2[2]){
+	//harjas add your code for side length here ******
+}
 
 float calculatePerimeter(float line1, float line2, float line3, float line4) {
 	return (line1 + line2 + line3 + line4);
@@ -96,27 +67,3 @@ float calculateArea(float line1, float line2, float line3, float line4) {
 	//in the case of a square same length is multiplied
 	return (line1 == line2) ? (line1 * line3) : (line1 * line2);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
