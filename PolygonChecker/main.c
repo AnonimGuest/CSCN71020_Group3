5e6e30384676628eae961d13164c8e4d2685cbc0
harjasb <harjasbrar5@gmail.com>
2024-03-09 14:21:43 -0500

    updated menu option to allow for user to enter x and y points, also added function that checks if the points are a rectangle (or valid, no letters etc.), points are then stored, able to be used by rectangle solver, further changes will be needed in main.c and rectangle solver in order to use the points properly

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -4,7 +4,7 @@
 #include "main.h"
 #include "triangleSolver.h"
 #include "rectangleSolver.h"
-
+#include <math.h>
 
 int main() {
 	bool continueProgram = true;
@@ -22,6 +22,20 @@ int main() {
 			break;
 		case 2:
 			printf_s("Rectangle selected.\n");
+                // Gather and check points for a rectangle
+                if (gatherAndCheckPoints(pointsArray)) {
+                    // Check if the points form a rectangle
+                    if (isRectangle(pointsArray)) {
+                        printf("The points form a rectangle.\n");
+
+                        // Additional logic or calculations for a rectangle can be added here
+
+                    } else {
+                        printf("The points do not form a rectangle.\n");
+                    }
+                } else {
+                    printf("Please enter integers for coordinates.\n");
+                }
 			break; 
 		case 0:
 			continueProgram = false;
@@ -89,5 +103,50 @@ double* getTriangleSides(double* triangleSides) {
 	printf("Triangle type: %s\n", triangleType);
 
 	return triangleSides;
+}
+
+bool gatherAndCheckPoints(int points[NUMBER_OF_POINTS][2]) {
+    printf("Enter coordinates for four points (x y):\n");
+
+    for (int i = 0; i < NUMBER_OF_POINTS; ++i) {
+        printf("Point %d: ", i + 1);
+
+        // Validate input for x coordinate
+        if (scanf("%d", &points[i][0]) != 1) {
+            printf("Invalid input for x coordinate.\n");
+            return false;
+        }
+
+        // Validate input for y coordinate
+        if (scanf("%d", &points[i][1]) != 1) {
+            printf("Invalid input for y coordinate.\n");
+            return false;
+        }
+    }
+
+    // Clear input buffer
+    while (getchar() != '\n');
+
+    return true;
+}
+
+// Function to check if the points form a rectangle
+bool isRectangle(int points[NUMBER_OF_POINTS][2]) {
+    // Implement the logic to check if the points form a rectangle
+
+    // Calculate distances between consecutive points
+    float side1 = sqrt(pow(points[0][0] - points[1][0], 2) + pow(points[0][1] - points[1][1], 2));
+    float side2 = sqrt(pow(points[1][0] - points[2][0], 2) + pow(points[1][1] - points[2][1], 2));
+    float side3 = sqrt(pow(points[2][0] - points[3][0], 2) + pow(points[2][1] - points[3][1], 2));
+    float side4 = sqrt(pow(points[3][0] - points[0][0], 2) + pow(points[3][1] - points[0][1], 2));
+
+    // Check if opposite sides are equal
+    bool oppositeSidesEqual = (side1 == side3) && (side2 == side4);
+
+    // Check if opposite sides are parallel (checking slopes)
+    bool oppositeSidesParallel = (points[1][0] - points[0][0]) * (points[2][1] - points[3][1]) ==
+                                 (points[2][0] - points[3][0]) * (points[1][1] - points[0][1]);
+
+    return oppositeSidesEqual && oppositeSidesParallel;
 }
 
