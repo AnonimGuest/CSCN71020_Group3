b408121fdf948a203c5539fdaa6453b72e57d062
LexBwmn <148971701+LexBwmn@users.noreply.github.com>
2024-03-11 10:10:31 -0400

    temporary

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
-
 #include "triangleSolver.h"
+
 #define SumAngles 180
 
-const char* analyzeTriangle(int side1, int side2, int side3) {
+const char* analyzeTriangle(double side1, double side2, double side3) {
 	const char* result = "";
 
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
@@ -23,8 +23,8 @@ const char* analyzeTriangle(int side1, int side2, int side3) {
 	{
 		result = "Isosceles triangle";
 
-		// angle calculation - Isosceles 
 
+		// angle calculation - Isosceles 
 		double cosA, BcosA, sinA, BsinA, r1, r2, r3, r4;
 
 		if (side1 == side2) {
@@ -41,35 +41,33 @@ const char* analyzeTriangle(int side1, int side2, int side3) {
 			printf("Angle 2: %.2lf\n", r2 * 180 / 3.14);
 			printf("Angle 3: %.2lf\n", r4);
 
-			}
-			else {
-				result = "Scalene triangle";
-				double cosA, BcosA, sinA, BsinA, r1, r2, r3, r4;
+		}
+		else {
+			result = "Scalene triangle";
+			double cosA, BcosA, sinA, BsinA, r1, r2, r3, r4;
 
-				// angle calculation -- Scalene 
+			// angle calculation -- Scalene 
 
-				// First Angle
-				cosA = pow(side3, 2) - pow(side1, 2) - pow(side2, 2);
-				BcosA = -2 * (side1) * (side2);
-				r1 = cosA / BcosA;
-				r2 = (acos(r1));
+			// First Angle
+			cosA = pow(side3, 2) - pow(side1, 2) - pow(side2, 2);
+			BcosA = -2 * (side1) * (side2);
+			r1 = cosA / BcosA;
+			r2 = (acos(r1));
 
-				//Second Angle
-				sinA = (sin(r2) * (side1));
-				BsinA = sinA / side3;
-				r3 = (asin(BsinA));
+			//Second Angle
+			sinA = (sin(r2) * (side1));
+			BsinA = sinA / side3;
+			r3 = (asin(BsinA));
 
-				printf("Angle 1: %.2lf\n", r2 * 180 / 3.14);
-				printf("Angle 2: %.2lf\n", r3 * 180 / 3.14);
+			printf("Angle 1: %.2lf\n", r2 * 180 / 3.14);
+			printf("Angle 2: %.2lf\n", r3 * 180 / 3.14);
 
-				// Thrid Angle
-				r4 = SumAngles - r2 * 180 / 3.14 - r3 * 180 / 3.14;
-				printf("Angle 3: %.2lf\n", r4);
+			// Thrid Angle
+			r4 = SumAngles - r2 * 180 / 3.14 - r3 * 180 / 3.14;
+			printf("Angle 3: %.2lf\n", r4);
 
-			}
-
-			return result;
 		}
 
+		return result;
 	}
-
+}
