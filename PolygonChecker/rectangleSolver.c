8b3ecba531faf4bf2006b6ecc8ff89b0599f7e77
andy-iio <andyguest12@gmail.com>
2024-03-11 21:09:19 -0400

    fixed a bug in the rectangle function

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -7,7 +7,7 @@
 #define RIGHTANGLE 90
 
 //function definitions
-void findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int top_left[2], int top_right[2]);
+bool findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int top_left[2], int top_right[2]);
 int calculateSideLength(int point1[2], int point2[2]);
 
 bool isRectangle(int points[4][2]) {
@@ -16,29 +16,29 @@ bool isRectangle(int points[4][2]) {
 	int top_left[2];
 	int top_right[2];
 
-	findCorners(points, bottom_left, bottom_right, top_left, top_right);
+	if (!findCorners(points, bottom_left, bottom_right, top_left, top_right)) {
+		return false;
+	}
+	else {
+		int top_side = calculateSideLength(top_left, top_right); //top side
+		int right_side = calculateSideLength(top_right, bottom_right); //right side
+		int bottom_side = calculateSideLength(bottom_right, bottom_left); //bottom side
+		int left_side = calculateSideLength(bottom_left, top_left); //left side
 
-	int side1 = calculateSideLength(top_left, top_right); //top side
-	int side2 = calculateSideLength(top_right, bottom_right); //right side
-	int side3 = calculateSideLength(bottom_right, bottom_left); //bottom side
-	int side4 = calculateSideLength(bottom_left, top_left); //left side
+		double diagonal1 = sqrt(top_side * top_side + right_side * right_side);
+		double diagonal2 = sqrt(bottom_side * bottom_side + left_side * left_side);
+		//printf("top_side: %d right_side: %d bottom_side: %d left_side: %d diag1: %f diag2: %f \n", top_side, right_side, bottom_side, left_side, sqrt(top_side * top_side + right_side * right_side), sqrt(bottom_side * bottom_side + left_side * left_side));
 
-	int diagonal1 = calculateSideLength(top_left, bottom_right);
-	int diagonal2 = calculateSideLength(top_right, bottom_left);
-
-	////NEED ANGLE CODE FROM TRIANGLE CODE TO CHECK FOR RIGHT ANGLES HERE
-
-	return (side1 == side3 && side2 == side4) && (diagonal1 == diagonal2) ;
+		return (top_side == bottom_side && right_side == left_side && top_side != right_side) && (diagonal1 == diagonal2);
+	}
 }
 
 //identifiying the corners
-void findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int top_left[2], int top_right[2]) {
-	printf("Points:\n");
-	for (int i = 0; i < NUMBEROFPOINTS; i++) {
-		printf("(%d, %d)\n", points[i][0], points[i][1]);
-	}
-	//top-left:min x, max y, top-right:max x, max y
-	//bottom-left:min x, min y, bottom-right: max x, min y
+bool findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int top_left[2], int top_right[2]) {
+	//printf("Points:\n");
+	//for (int i = 0; i < NUMBEROFPOINTS; i++) {
+	//	printf("(%d, %d)\n", points[i][0], points[i][1]);
+	//}
 	//setting initial min and max coordinates
 	int minX = points[0][0];
 	int minY = points[0][1];
@@ -53,20 +53,39 @@ void findCorners(int points[4][2], int bottom_left[2], int bottom_right[2], int 
 		maxY = points[array_counter][1] > maxY ? points[array_counter][1] : maxY;
 	}
 
-	//set the corners values 
-	bottom_left[0] = minX;
-	bottom_left[1] = minY;
-	bottom_right[0] = maxX;
-	bottom_right[1] = minY;
-	top_left[0] = minX;
-	top_left[1] = maxY;
-	top_right[0] = maxX;
-	top_right[1] = maxY;
-
+	//top-left:min x, max y, top-right:max x, max y
+    //bottom-left:min x, min y, bottom-right: max x, min y
+	for (int array_counter = 0; array_counter < NUMBEROFPOINTS; array_counter++) {
+		if (points[array_counter][0] == minX && points[array_counter][1] == maxY) { //top-left
+			top_left[0] = points[array_counter][0];
+			top_left[1] = points[array_counter][1];
+		}
+		else if ((points[array_counter][0] == maxX && points[array_counter][1] == maxY)) { //top-right
+			top_right[0] = points[array_counter][0];
+			top_right[1] = points[array_counter][1];
+		}
+		else if ((points[array_counter][0] == maxX && points[array_counter][1] == minY)) { //bottom-right
+			bottom_right[0] = points[array_counter][0];
+			bottom_right[1] = points[array_counter][1];
+		}
+		else if ((points[array_counter][0] == minX && points[array_counter][1] == minY)) { //bottom-left
+			bottom_left[0] = points[array_counter][0];
+			bottom_left[1] = points[array_counter][1];
+		}
+		else {
+			return false;
+		}
+	}
+	return true;
+	//printf("corners %d %d %d %d %d %d %d %d\n", bottom_left[0], bottom_left[1], bottom_right[0], bottom_right[1], top_left[0], top_left[1], top_right[0], top_right[1]);
 }
 
-int calculateSideLength(int point1[2], int point2[2]){
-	return sqrt(pow((point2[0] - point1[0]), 2) + pow((point2[1] - point1[1]), 2));
+int calculateSideLength(int point1[2], int point2[2]) {
+	return sqrt(pow((point1[0] - point2[0]), 2) + pow((point1[1] - point2[1]), 2));
+}
+
+double calculateDiagonal(double line1, double line2) {
+	return sqrt(line1 * line1 + line2 * line2);
 }
 
 int calculatePerimeter(int line1, int line2, int line3, int line4) {
@@ -75,6 +94,5 @@ int calculatePerimeter(int line1, int line2, int line3, int line4) {
 
 int calculateArea(int line1, int line2, int line3, int line4) {
 	//checks to make sure the different length sides are multiplied
-	//in the case of a square same length is multiplied
 	return (line1 == line2) ? (line1 * line3) : (line1 * line2);
 }
--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include "triangleSolver.h"
+#include <math.h>
 
-const char* analyzeTriangle(double side1, double side2, double side3) {
+#include "triangleSolver.h"
+#define SumAngles 180
+
+
+
+const char* analyzeTriangle(int side1, int side2, int side3) {
 	const char* result = "";
 
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
@@ -10,12 +15,61 @@ const char* analyzeTriangle(double side1, double side2, double side3) {
 	}
 	else if (side1 == side2 && side1 == side3) {
 		result = "Equilateral triangle";
+
+		// angle calculation -- Equilateral 
+		int angles = SumAngles / 3;
+		printf("Angles: %d\n", angles);
 	}
-	else if ((side1 == side2 && side1 != side3) || (side1 == side3 && side1 != side2)) {
+	else if ((side1 == side2 && side1 != side3) ||
+		(side1 == side3 && side1 != side2))
+	{
 		result = "Isosceles triangle";
+
+		// angle calculation - Isosceles 
+
+		double cosA, BcosA, sinA, BsinA, r1, r2, r3, r4;
+
+		if (side1 == side2) {
+			// first and second angle (same)
+			cosA = pow(side3, 2) + pow(side1, 2) - pow(side2, 2);
+			BcosA = 2 * (side3) * (side1);
+			r1 = cosA / BcosA;
+			r2 = (acos(r1));
+
+			// calculation for the other angle (different)
+			r4 = SumAngles - 2 * (r2 * 180 / 3.14);
+
+			printf("Angle 1: %.2lf\n", r2 * 180 / 3.14);
+			printf("Angle 2: %.2lf\n", r2 * 180 / 3.14);
+			printf("Angle 3: %.2lf\n", r4);
+
+		}
+		else {
+			result = "Scalene triangle";
+			double cosA, BcosA, sinA, BsinA, r1, r2, r3, r4;
+
+			// angle calculation -- Scalene 
+
+			// First Angle
+			cosA = pow(side3, 2) - pow(side1, 2) - pow(side2, 2);
+			BcosA = -2 * (side1) * (side2);
+			r1 = cosA / BcosA;
+			r2 = (acos(r1));
+
+			//Second Angle
+			sinA = (sin(r2) * (side1));
+			BsinA = sinA / side3;
+			r3 = (asin(BsinA));
+
+			printf("Angle 1: %.2lf\n", r2 * 180 / 3.14);
+			printf("Angle 2: %.2lf\n", r3 * 180 / 3.14);
+
+			// Thrid Angle
+			r4 = SumAngles - r2 * 180 / 3.14 - r3 * 180 / 3.14;
+			printf("Angle 3: %.2lf\n", r4);
+			
+
+		}
+		return result;
 	}
-	else {
-		result = "Scalene triangle";
-	}
-	return result;
 }
